bf55dc5da40233c57774a611d131ff96c6f468cc
xiaoyi.qian <xiaoyi.qian@cohereny.global>
2021-09-13 11:06:50 +0800

    ts

--- src/login/assets/js/test.ts
+++ src/login/assets/js/test.ts
@@ -1,6 +1,8 @@
-const meaagse: string = "ts测试";
-const connentString: string = meaagse+"一下";
+const meaagse: String = "ts测试";
+const connentString: String = meaagse+"一下";
+const userList: Array<string> =  ['小明','小红'];
 export default function getTs (){
     console.log(connentString)
+    console.log(userList,'user.....')
 }
 
